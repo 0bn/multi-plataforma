ab0a69f3cbd7b753ee62204c9fb98c58a734d110
marcos pedro <70784710+0bn@users.noreply.github.com>
2024-09-13 12:02:54 -0300

    Aaaaaa

---
+++ flutter/exibir_imagens/teste_json.dart
@@ -0,0 +1,64 @@
+// import 'dart:convert';
+
+// class PessoaModel{
+//     String nome;
+//     int idade;
+
+//     PessoaModel(this.nome, this.idade);
+
+//     // //construtor nomeado
+//     // PessoaModel.fromJSON(decodedJSON){
+//     //     nome
+//     // }
+
+    // //recebe uma string e devolve um mapa 
+    // var pessoaDecoded = json.decode(pessoaJSON);
+
+    // var pessoa = PessoaModel(pessoaDecoded['nome'], pessoaDecoded['idade']);
+
+    // print(pessoa.idade);
+    // print(pessoa.nome);
+
+    // }
+
+//adicione preco a cada veiculo
+//calcule e mostre a media de preco
+
+// void main(){
+//     var veiculosJSON = '''
+//         [{
+//             "marca": "VW",
+//             "modelo": "Gol"
+//             "preco": "12000"
+
+//         },
+//         {
+//             "marca": "GM",
+//             "modelo": "Onix"
+//             "preco": "30000"
+
+//         }
+//     ]
+//     ''';
+
+
+
+//     // var veiculos = json.decode(veiculosJSON);
+//     // print(veiculos.runtimeType);
+//     // var media = 0.0;
+//     // for (final veiculo in veiculos){
+//     //     print(veiculo);
+//     //     print(veiculo['marca']);
+//     //     print(veiculo['modelo']);
+//     //     media = media + veiculo['preco'];
+//     // }
+
+//     // print((media/veiculos.length).toStringasFixed(2));
+
+//     // var pessoaJSON = '{"nome": "Ana", "idade": 18}'
+//     // var pessoa = json.decode(pessoaJSON);
+//     // print(pessoa.runtimeType);
+//     // print(pessoa['nome']);
+//     // print(pessoa['idade']);
+// }
+// }
