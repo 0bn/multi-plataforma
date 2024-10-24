4c0266167202f083d81445026381d5f686aee922
marcos pedro <70784710+0bn@users.noreply.github.com>
2024-10-24 08:16:26 -0300

    peguei do prof

--- flutter/gerenciamento_de_estado/lib/src/app.dart
+++ flutter/gerenciamento_de_estado/lib/src/app.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gerenciamento_de_estado/src/telas/login_tela.dart';
 
-class App extends StatelessWidget {
+class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Login',
-        home: Scaffold(
-          body: LoginTela(),
-        )
-      );
-    }
+      title: 'Login',
+      home: Scaffold(
+        body: LoginTela(),
+      )
+    );
   }
-
+}
--- flutter/gerenciamento_de_estado/lib/src/telas/login_tela.dart
+++ flutter/gerenciamento_de_estado/lib/src/telas/login_tela.dart
@@ -1,48 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-class LoginTela extends StatelessWidget {
+import '../blocs/bloc.dart';
+class LoginTela extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(20.0),
+      margin: const EdgeInsets.all(20.0),
       child: Column(
+        //4. Encaixar os dois métodos, ou seja, exibir os dois campos
         children: [
           emailField(),
           passwordField(),
-          submitButton(),
+          Container(
+            margin: EdgeInsets.only(top: 12.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: submitButton()
+                )
+              ],
+            ),
+          )
+            
         ],
       ),
     );
   }
 
-  Widget emailField() {
-    //restringir o teclado para que ele seja apropriado para a digitação de email
-    //exibir uma dica instruindo o usuadio a digitar o seu email
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        hintText: 'digite seu email',
-      ),
-    );
-  }
-
-  //metodo que reproduz um widget proprio para exibicao de campo de senha
-  Widget passwordField() {
-    return TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'digite sua senha',
-      ),
-    );
-  }
-
-  Widget submitButton() {
-    return ElevatedButton(
-      onPressed: () {
-        // acao
+  //1. restringir o teclado para que ele seja apropriado para a digitação de e-mail
+  //2. Exibir uma dica instruindo o usuário a digitar o seu e-mail
+  Widget emailField(){
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, AsyncSnapshot <String> snapshot){
+        return TextField(
+          // onChanged: bloc.changeEmail
+          onChanged: (valorDigitado){
+            bloc.changeEmail(valorDigitado);
+          },
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'seu@email.com',
+            labelText: 'Digite seu e-mail',
+            //se tem erro, mosdtrar o erro, senão, não mostrar nada
+            //use um operador ternário
+            // errorText: snapshot.hasError ? snapshot.error.toString() : null
+            errorText: snapshot.error?.toString()
+          ),
+        );
       },
-      child: Text('login'),
+    );
+  }
+  //3. Escrever um método semelhante ao método emailField que produz um Widget próprio para a exibição de um campo para senha
+  Widget passwordField(){
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context,  AsyncSnapshot <String> snapshot){
+        return TextField(
+          onChanged: bloc.changePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: 'Senha',
+            labelText: 'Digite sua senha',
+            errorText: snapshot.error?.toString()
+          ),
+        );
+      },
+    );
+    
+    
+    
+  }
+  //5. implementar um método para produzir um botão
+  Widget submitButton(){
+    return ElevatedButton(
+      onPressed: (){}, 
+      child: Text('Login')
     );
   }
 }
